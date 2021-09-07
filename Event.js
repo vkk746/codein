7ae2022193adc46a9a840364340e136ac407191a
vkk746 <vs5090506@gmail.com>
2021-09-07 14:08:10 +0530

    related Event

---
+++ Event.js
@@ -0,0 +1,35 @@
+//HTML event is that things that happen in the html elements
+//when javascript is used in html pages javascript can react on html
+// HTML events
+// An HTML event can be something browser does, or something user does
+// html is allows event handler atribute with javascript code to be added to html elements
+
+// four ways to writing events in javascript 
+// 1-using alert() mathod
+// 2-By calling a function(we already seen west way to writing function)
+//3- Using inline events(HTML on click="property and element.click")
+//4-Using Event Listioners()
+
+// what is event object
+// like MouseEvent, focusEvent, keyboardEvent etc
+//keyboard events
+//keypress and keydown events hai keyboard event k
+//event.kye se hame yh pata chal jata hai ki kaun si key press karni hai
+
+
+//Timing bassed events in javascript
+//setTimeout()
+//setInterval()
+//clearTimeout()
+//clearInterval();
+//The window object allows the execution of code at specified time intevals
+//The Time interval called timing event
+//two key mathod are used in javascript
+setTimeout(function,millisecond);
+//execution of function after that waiting given millisecond
+setInterval(function,millisecond);
+// same as setTimeout,but repeats the excution of the function contineusly
+//clearTimeout() se ham stop karte
+//setInterval ko stop clearInterval() se karte hai
+
+
